d81a2424ecf732fe6b90aa393a4ad66cd76a2552
Tim <timothynn08@gmail.com>
2023-01-16 22:23:03 +0300

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 @main def hello: Unit = 
   println("Hello world!")
   println(msg)
+  println(helloworld)
 
 def msg = "I was compiled by Scala 3. :)"
+def helloworld = "I am making scala snippets for future use"
